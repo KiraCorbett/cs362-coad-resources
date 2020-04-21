3c9fb7112525129424a60d891645381526ecee75
Kira Corbett <kiracorbett00@gmail.com>
2020-04-20 17:14:59 -0700

    added characterization test 2.4 for first model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-	it "exists" do
-		Organization.new()
+	it "expect name responds to corresponding attribute" do
+		expect(Organization).to respond_to(:name)
 	end
 
 end
