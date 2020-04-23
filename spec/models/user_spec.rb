aac1cad15cbf1994272a7cb17a6b264daf818140
Kira Corbett <kiracorbett00@gmail.com>
2020-04-23 09:06:14 -0700

    added user test attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-	it "expect name responds to corresponding attribute" do
-		expect(User).to respond_to(:name)
+	let(:User) { User.new(email: 'test@test.com', encrypted_password: '123456abcdef', role: '')}
+
+	describe "attributes" do
+	
+		it "has email" do
+			User = User.new
+			expect(User).to respond_to(:email)
+		end
+
+		it "has password" do
+			expect(User).to respond_to(:password)
+		end
+
+		it "has role" do
+			expect(User).to respond_to(:role)
+		end
 	end
 
 end
