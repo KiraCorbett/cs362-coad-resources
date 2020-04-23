ae523fb8d03e1f302d8a5683aba4c5d7bdc28d34
Kira Corbett <kiracorbett00@gmail.com>
2020-04-23 09:31:02 -0700

    validation test: validate password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe User, type: :model do
 
 	# properties / attributes
 	describe "attributes" do
-		it "has email" do
+		it "has an email" do
 			expect(user).to respond_to(:email)
 		end
 
-		it "has password" do
+		it "has a password" do
 			expect(user).to respond_to(:password)
 		end
 
-		it "has role" do
+		it "has a role" do
 			expect(user).to respond_to(:role)
 		end
 	end
@@ -31,6 +31,17 @@ RSpec.describe User, type: :model do
 		it "is valid with an email" do
 			expect(user).to validate_presence_of(:email)
 		end
+
+		it "is a valid password length" do
+			expect(user).to validate_length_of(:password).is_at_least(7).is_at_most(255)
+		end
+
+		# see user.rb for these validations that need to be tested
+		# validates_length_of :email, minimum: 1, maximum: 255, on: :create
+		# validates :email, format: { with: VALID_EMAIL_REGEX }
+		# validates_uniqueness_of :email, case_sensitive: false
+		# validates_presence_of :password, on: :create
+		# validates_length_of :password, minimum: 7, maximum: 255, on: :create
 	end
 
 	# methods
