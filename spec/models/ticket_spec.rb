0c92c7cf6928017fbc6a54ab62e7688db678a835
unknown <amberj.kolar@gmail.com>
2020-04-28 11:50:15 -0700

    Added incorrect attribute tests to ticket_spec.rb

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Ticket, type: :model do
 		expect(Ticket).to respond_to(:name)
 	end
 
+	# properties / attributes
+	describe "attributes" do
+		specify{ expect(ticket).to respond_to(:name) }
+		specify{ expect(ticket).to respond_to(:description) }
+		specify{ expect(ticket).to respond_to(:phone) }
+		specify{ expect(ticket).to respond_to(:closed) }
+		specify{ expect(ticket).to respond_to(:closed_at) }
+	end
+
 end
