676ba3a9c9cbab83f0aa3549fefdc525f656d54f
Kira Corbett <kiracorbett00@gmail.com>
2020-04-28 11:36:30 -0700

    attribute tests for organization_spec

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
-	it "expect name responds to corresponding attribute" do
-		expect(Organization).to respond_to(:name)
-	end
+	let(:organization) { Organization.new()}
 
+	# properties / attributes
+	describe "attributes" do
+		specify{ expect(organization).to respond_to(:name) }
+		specify{ expect(organization).to respond_to(:status) }
+		specify{ expect(organization).to respond_to(:phone) }
+		specify{ expect(organization).to respond_to(:email) }
+		specify{ expect(organization).to respond_to(:description) }
+		specify{ expect(organization).to respond_to(:rejection_reason) }
+		specify{ expect(organization).to respond_to(:liability_insurance) }
+		specify{ expect(organization).to respond_to(:primary_name) }
+		specify{ expect(organization).to respond_to(:secondary_name) }
+		specify{ expect(organization).to respond_to(:secondary_phone) }
+		specify{ expect(organization).to respond_to(:title) }
+		specify{ expect(organization).to respond_to(:transportation) }
+	end
 end
