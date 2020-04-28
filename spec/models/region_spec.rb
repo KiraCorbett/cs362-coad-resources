f4b66aebf860df0125896a79a8f7dbdcc619996b
Kira Corbett <kiracorbett00@gmail.com>
2020-04-28 11:29:11 -0700

    attribute tests for resource_category

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-	it "expect name responds to corresponding attribute" do
-		expect(Region).to respond_to(:name)
+	describe "attributes" do
+		it "has a name" do
+			expect(Region).to respond_to(:name)
+		end
 	end
 
 end
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-	it "expect name responds to corresponding attribute" do
-		expect(ResourceCategory).to respond_to(:name)
+	describe "attributes" do
+		it "has a name" do
+			expect(ResourceCategory).to respond_to(:name)
+		end
+
+		it "is active" do
+			expect(ResourceCategory).to respond_to(:active)
+		end
 	end
 
 end
