abc91684ea1c136a5407c8d0e86749b13776bb26
Kira Corbett <kiracorbett00@gmail.com>
2020-04-30 17:45:37 -0700

    fixed ticket_spec to pass

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
 
-	it "expect name responds to corresponding attribute" do
-		expect(Ticket).to respond_to(:name)
-	end
+	let(:ticket) { Ticket.new }
 
 	# properties / attributes
 	describe "attributes" do
@@ -14,5 +12,4 @@ RSpec.describe Ticket, type: :model do
 		specify{ expect(ticket).to respond_to(:closed) }
 		specify{ expect(ticket).to respond_to(:closed_at) }
 	end
-
 end
