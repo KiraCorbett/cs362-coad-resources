288c866620013fd1fa6ba310a033a00892b98f21
Kira Corbett <kiracorbett00@gmail.com>
2020-05-07 09:57:46 -0700

    Replicated resource_category unspecified method test

--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
 	factory :region do
 		name {'FAKE'}
 	end
+
+	trait :unspecified do
+      name { 'Unspecified' }
+    end
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Region, type: :model do
     end
   end
 
+  describe "::unspecified" do
+    
+    it "creates a new Unspecified resource category when one does not exist" do
+      expect(Region.where(name: 'Unspecified')).to be_empty
+      expect{ Region.unspecified }.to change { Region.count }.by(1)
+    end
+    it "does not create a new Unspecified resource category when one already exists" do
+      create(:region, :unspecified)
+      expect{ Region.unspecified }.to_not change { Region.count }
+    end
+    it "returns a resource category with the name unspecified" do
+      expect(Region.unspecified.name).to eq('Unspecified')
+    end
+
+  end
+
 end
