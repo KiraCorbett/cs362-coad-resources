80a9d82d50842c8d139cb193706d5d751318e3bf
Kira Corbett <kiracorbett00@gmail.com>
2020-05-12 09:36:50 -0700

    refactoring shoulda validators

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Organization, type: :model do
     specify{ expect(organization).to respond_to(:status) }
     specify{ expect(organization).to respond_to(:phone) }
     specify{ expect(organization).to respond_to(:email) }
-    specify{ expect(organization).to respond_to(:description) }
+    #specify{ expect(organization).to respond_to(:description) }
     # specify{ expect(organization).to respond_to(:rejection_reason) }
     # specify{ expect(organization).to respond_to(:liability_insurance) }
     specify{ expect(organization).to respond_to(:primary_name) }
@@ -36,40 +36,19 @@ RSpec.describe Organization, type: :model do
   end
 
   describe "validations" do
- 
-    it "has a valid name" do
-      expect(organization).to validate_presence_of(:name)
-    end
-
-    it "has a valid status" do
-      expect(organization).to validate_presence_of(:status)
-    end
-
-    it "has a valid phone" do
-      expect(organization).to validate_presence_of(:phone)
-    end
-
-    it "has a valid email" do
-      expect(organization).to validate_presence_of(:email)
-    end
-
-    it "has a valid primary name" do
-      expect(organization).to validate_presence_of(:primary_name)
-    end
-
-    it "has a valid secondary name" do
-      expect(organization).to validate_presence_of(:secondary_name)
-    end
-
-    it "has a valid secondary phone" do
-      expect(organization).to validate_presence_of(:secondary_phone)
-    end
- 
-     it "has a valid name length" do
-      expect(organization).to validate_length_of(:name).at_least(1).at_most(255).on(:create)
-    end
-
-
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:primary_name) }
+    it { should validate_presence_of(:secondary_name) }
+    it { should validate_presence_of(:secondary_phone) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
   end
+
+    # it "is a valid email" do
+    #   expect(organization).to validate(:email).format(with: VALID_EMAIL_REGEX)
+    # end
 
 end
