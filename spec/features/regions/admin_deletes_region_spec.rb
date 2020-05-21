37bb17a9ff20b9481f4401606875788fe2b04e29
Kira Corbett <kiracorbett00@gmail.com>
2020-05-21 15:20:26 -0700

    feature admin_deletes demo test demo

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+	let(:admin) do
+		User.create(email: 'test@test.com', password: 'password',
+					password_confirmation: 'password', role: 'admin')
+	end
+	let(:region) do
+
+
+	end
+	before do
+		admin.confirm
+		log_in_as(admin)
+	end
+
+	context 'as an admin' do
+		it 'deletes a region' do
+			region = Region.create(name: 'Fake Region')
+			visit region_path(region)
+			click_on 'Delete'
+			expect(page).to have_content('Regions')
+			expect(page).to have_content('was deleted')
+		end
+	end
 
 end
