6d16929544a308726b78f2f101c4222de35a4c61
Kira Corbett <kiracorbett00@gmail.com>
2020-05-26 15:23:15 -0700

    added organization user controller test

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -16,22 +16,37 @@ RSpec.describe RegionsController, type: :controller do
 
     context 'As an organization user' do
         let(:user) { create(:user) }
+        let(:region) { create(:region) }
 
-        before do   
-            sign_in(user)
-        end
+        before(:each) { sign_in(user) }
 
         specify 'GET #index' do
-            expect(get(:new)).to redirect_to(dashboard_path) 
+            expect(get(:index)).to redirect_to(dashboard_path) 
         end
         describe 'GET #new' do
 			specify { expect(get(:new)).to redirect_to(dashboard_path)}
+		end
+		describe 'GET #edit' do
+			specify { expect(get(:edit, params: {:id => 1 })).to redirect_to(dashboard_path) }
+		end
+		describe 'GET #show' do
+			specify { expect(get(:show, params: {:id => 1 })).to redirect_to(dashboard_path) }
+		end
+		describe 'POST #create' do
+			specify { expect(post(:create, params: {:id => 1 })).to redirect_to(dashboard_path) }
+		end
+		describe 'PUT #update' do
+			specify { expect(put(:update, params: {:id => 1 })).to redirect_to(dashboard_path) }
+		end
+		describe 'DELETE #destroy' do
+			specify { expect(delete(:destroy, params: {:id => 1 })).to redirect_to(dashboard_path) }
 		end
     end
 
 	context 'As an admin' do
 		let(:admin_user) { create(:user, :admin) }
 		let(:region) { create(:region) }
+
 		before(:each) { sign_in(admin_user) }
 
 		describe 'GET #index' do
