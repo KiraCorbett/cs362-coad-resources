93f3ea125c2bffc9df85b509650d78e8dd323477
Kira Corbett <kiracorbett00@gmail.com>
2020-05-26 14:39:39 -0700

    init update_organization feature from class

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
 
+
+	let(:organization) { create(:organization) }
+
+	context "As an admin" do
+		it "updating an organization successfully" do
+			visit edit_organization_path(organization)
+		end
+	end
 end
