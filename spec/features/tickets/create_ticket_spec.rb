f110dcd878c562f87b834c51424a9bc606f7522c
Kira Corbett <kiracorbett00@gmail.com>
2020-05-27 14:48:36 -0700

    unhappy create ticket feature test

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -19,6 +19,17 @@ RSpec.describe 'Creating a Ticket', type: :feature do
 			visit ticket_submitted_path
 			expect(page).to have_text("Ticket Submitted")
 		end
+
+		it "does not successfully create a ticket" do 
+			visit new_ticket_path
+			fill_in "ticket_name", with: ''
+			fill_in "ticket_phone", with: '15416893012'
+			select 'Select', from: 'ticket_region_id'
+			select 'Select', from: 'ticket_resource_category_id'
+			fill_in "ticket_description", with: 'Fake description'
+			click_on "Send this help request"
+			expect(page).not_to have_text("Ticket Submitted")
+		end
 	end
 end
 
