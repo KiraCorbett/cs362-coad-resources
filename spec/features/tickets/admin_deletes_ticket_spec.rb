5089524ee79a3090f9631f8fedc037e47e750284
Kira Corbett <kiracorbett00@gmail.com>
2020-05-27 14:37:57 -0700

    feature test for admin deletes ticket path

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
 
+	let(:admin) { create(:user, :admin) }
+	let(:ticket) { create(:ticket, :id) }
+
+	before :each do
+		admin.confirm
+		log_in_as(admin)
+		ticket.id
+	end
+
+	context "as an admin" do
+		it "deletes a ticket successfully" do
+			visit ticket_path(ticket)
+			click_on "Delete"
+			expect(page).to have_content("was deleted")
+		end
+	end
 end
