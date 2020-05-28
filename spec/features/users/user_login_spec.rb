c5eb86e73554e38d571df3c75132cd5f55418d38
Kira Corbett <kiracorbett00@gmail.com>
2020-05-27 20:22:22 -0700

    happy path for successful user login

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+	let(:user) { create(:user) }
+
+	before :each do
+		user.confirm # this is already done in the user factory
+	end
+
+	context "As an organization user" do
+		it "successfully logins in user" do
+			visit login_path
+			fill_in "user_email", with: user.email
+			fill_in "user_password", with: user.password
+			click_on "commit"
+			expect(page).to have_content("Dashboard")
+		end
+	end
 end
