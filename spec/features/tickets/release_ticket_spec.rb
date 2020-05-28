9cc6163add79bc95bcdf9e9da23bffc877ae141f
Kira Corbett <kiracorbett00@gmail.com>
2020-05-28 09:19:56 -0700

    org user releases ticket feature test

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
+	let(:organization) { create(:organization, :approved) }
+	let(:user) { create(:user, organization: organization) }
+	let(:ticket) { create(:ticket, organization: organization) }
 
+	context "As an approved organization user" do
+		it "releases a ticket successfully" do
+			log_in_as(user)
+			visit ticket_path(ticket.id)
+			click_on "Release"
+			expect(page).to have_content("Dashboard")
+		end
+	end
 end
