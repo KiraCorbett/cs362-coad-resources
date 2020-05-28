941052d3e731e4d63cf064f5bb72b88d60e9c804
Kira Corbett <kiracorbett00@gmail.com>
2020-05-28 09:03:02 -0700

    org user captures ticket feature test

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,24 +1,16 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Capturing a ticket', type: :feature do
-# 	let(:organization) { create(:organization, :approved) }
-# 	let(:user) { create(:user, organization: organization) }
-# 	let(:ticket) { create(:ticket, organization: user.organization) }
+RSpec.describe 'Capturing a ticket', type: :feature do
+	let(:organization) { create(:organization, :approved) }
+	let(:user) { create(:user, organization: organization) }
+	let(:ticket) { create(:ticket) }
 
-# 	context "As an approved organization user" do
-# 		it "captures a ticket successfully" do
-# 			log_in_as(user)
-# 			visit ticket_path(ticket.id.captured?)
-# 			click_on "Capture"
-# 			expect(page).to have_content(ticket.page)
-# 		end
-# 	end
-# end
-
-
-# # - approved organization user
-# # - organization user with approved
-# # - ticket in system
-# # - sign in as organization user
-# # - click on a ticket 
-# # - capture it
+	context "As an approved organization user" do
+		it "captures a ticket successfully" do
+			log_in_as(user)
+			visit ticket_path(ticket.id)
+			click_on "Capture"
+			expect(current_path).to eq(dashboard_path)
+		end
+	end
+end
