226d938a0027746883c5c323f46c48f4ef9021f0
Kira Corbett <kiracorbett00@gmail.com>
2020-06-01 21:51:57 -0700

    started resource categories controller test

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  let(:resource_category) { create(:resource_category) }
 
+  before do
+    resource_category
+  end
+
+  context 'As a non logged-in user' do
+    describe 'GET #index' do
+      specify { expect(get(:index)).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'GET #new' do
+      specify { expect(get(:new)).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'GET #create' do
+      specify { expect(get(:create, params: {:id => 1 })).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'GET #update' do
+      specify { expect(get(:update, params: {:id => 1 })).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'GET #activate' do
+      specify { expect(get(:activate, params: {:id => 1 })).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'GET #deactivate' do
+      specify { expect(get(:deactivate, params: {:id => 1 })).to redirect_to(new_user_session_path) }
+    end
+
+    describe 'GET #destroy' do
+      specify { expect(get(:destroy, params: {:id => 1 })).to redirect_to(new_user_session_path) }
+    end
+
+  end
 end
